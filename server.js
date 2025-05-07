1320ac0d9ffc8ae1c069f494dd8eab07d1d33c96
danieldavidps94 <dandasilva@firjan.com.br>
2025-05-07 08:59:26 -0300

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,8 @@ import { PDFDocument, StandardFonts, rgb } from 'pdf-lib';
 dotenv.config();
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
 const PORT = process.env.PORT || 3000;
 
