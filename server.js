c3e8b324f1413cefe97805879ccb8481de2fc666
danieldavidps94 <dandasilva@firjan.com.br>
2025-05-07 09:28:09 -0300

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.use(cors());
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 10000;
 const GITHUB_OWNER = 'danieldavidps94';
 const GITHUB_REPO = 'formularios-firjan';
 
