7e5e2d727651b00390636b01a8db0598d5e36661
danieldavidps94 <dandasilva@firjan.com.br>
2025-05-08 13:52:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -25,12 +25,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors({
-  origin: 'https://danieldavidps94.github.io',
-  methods: ['GET', 'POST', 'OPTIONS'],
-  allowedHeaders: ['Content-Type', 'Authorization'],
-}));
-
 app.use(express.json({ limit: '10mb' }));
 app.use(express.urlencoded({ extended: true, limit: '10mb' }));
 app.set('view engine', 'ejs');
